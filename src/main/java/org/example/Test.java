39b893d0c50ed04b16918bf1a137afbfb3baa28f
Artyom Badoyan <artyom.styopayevich@gmail.com>
2023-01-03 21:22:35 +0400

    Bean scope singltone

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -2,17 +2,26 @@ package org.example;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test {
+public class Test1 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Person person = context.getBean("myPerson", Person.class);
+        Dog myDog = context.getBean("myPet", Dog.class);
+        myDog.setName("Belka");
+        Dog yourDog = context.getBean("myPet", Dog.class);
+        yourDog.setName("Strelka");
 
-        System.out.println(person.getSurname());
-        System.out.println(person.getAge());
+        System.out.println(myDog.getName());
+        System.out.println(yourDog.getName());
 
-        person.callYourPet();
+        System.out.println("переменные ссиляются на один и тот же объект? "
+                + (myDog == yourDog));
+
+        System.out.println(myDog);
+        System.out.println(yourDog);
+
         context.close();
+
     }
 }
